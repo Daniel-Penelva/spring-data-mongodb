d696c209712c1644664a8fea7d39c6ae67b3fde9
Daniel Penelva <d4n.andrade@gmail.com>
2023-10-04 20:12:38 -0300

    Criando método para adicionar Funcionario ao Projeto

--- src/main/java/com/daniel/springdatamongodb/controller/ProjetoController.java
+++ src/main/java/com/daniel/springdatamongodb/controller/ProjetoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +35,15 @@ public class ProjetoController {
     public ResponseEntity<List<Projeto>> obterTodosProjetos(){
         List<Projeto> projetos = projetoService.obterTodosProjetos();
         return new ResponseEntity<>(projetos, HttpStatus.OK);
+    }
+
+
+    // http://localhost:8080/api/projetos/{projetoId}/adicionar-funcionario/{funcionarioId}
+    @PostMapping("/{projetoId}/adicionar-funcionario/{funcionarioId}")
+    public ResponseEntity<Projeto> criarFuncionarioAoProjeto(@PathVariable String projetoId, @PathVariable String funcionarioId){
+
+        Projeto projeto = projetoService.adicionarFuncionarioAoProjeto(projetoId, funcionarioId);
+        return new ResponseEntity<>(projeto, HttpStatus.OK);
     }
     
 }
